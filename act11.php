e29f3c5d6fc231bd43fa2f5feab6c04743a20f4b
Sandro Comes Zacares <sacoza@floridauniversitaria.es>
2016-12-15 10:11:04 +0100

    first commit

---
+++ act11.php
@@ -0,0 +1,27 @@
+<?php
+
+class JugadorDado
+{
+private $minNumDado=0;
+private $maxNumDado=0;
+public $tirarDado=0;
+
+public function setminNumDado(){
+$this->minmundado>0;
+}
+public function getminNumDado(){
+return $this->minnumdado;
+}
+
+public function setmaxNumDado(){
+$this->maxmundado<13;
+}
+public function getmaxNumDado(){
+return $this->maxnumdado;
+}
+public function RandomPos() {
+  $this->tirarDado=rand(1,12);
+  }
+
+}
+?>
